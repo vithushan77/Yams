187c375b884b665f997ba34e2b2fc9e60cad4d03
vithushan <72071557+vithushan77@users.noreply.github.com>
2023-12-21 09:28:47 +0100

    ajout not yams

--- yams.py
+++ yams.py
@@ -1,15 +1,15 @@
 predefined_rolls = [
-    [6, 6, 6, 6, 6],  # Yams 0
-    [4, 4, 4, 4, 2],  # Carré 1
-    [3, 3, 3, 2, 2],  # Full 2
-    [1, 2, 3, 4, 6],  # Petite Suite 3
-    [2, 3, 4, 5, 1],  # Petite Suite 4
-    [1, 2, 3, 4, 5],  # Grande Suite 5
-    [2, 3, 4, 5, 6],  # Grande Suite 6
-    [2, 2, 2, 4, 5],  # Brelan 7
-    [1, 3, 4, 5, 6],  # Chance 8
-    [1, 2, 2, 4, 5],  # Non gagnant 9
-    [1, 3, 4, 4, 6],  # Non gagnant 10
+    [6, 6, 6, 6, 6],  # Yams
+    [4, 4, 4, 4, 2],  # Carré
+    [3, 3, 3, 2, 2],  # Full
+    [1, 2, 3, 4, 6],  # Petite Suite
+    [2, 3, 4, 5, 1],  # Petite Suite
+    [1, 2, 3, 4, 5],  # Grande Suite
+    [2, 3, 4, 5, 6],  # Grande Suite
+    [2, 2, 2, 4, 5],  # Brelan
+    [1, 3, 4, 5, 6],  # Chance
+    [1, 2, 2, 4, 5],  # Non gagnant
+    [1, 3, 4, 4, 6],  # Non gagnant
 ]
 
 
@@ -24,21 +24,25 @@ def is_yams(dice_roll):
 def is_brelan(dice_roll):
     for die in set(dice_roll):
         if dice_roll.count(die) >= 3:
-            return 1
+            return 28
     return 0
 
 def is_carre(dice_roll):
     for die in set(dice_roll):
         if dice_roll.count(die) >= 4:
-            return 1
+            return 35
     return 0
 def is_full(dice_roll):
     dice_count = {die: dice_roll.count(die) for die in set(dice_roll)}
-    return 3 in dice_count.values() and 2 in dice_count.values()
+    if 3 in dice_count.values() and 2 in dice_count.values():
+        return 30
+    return 0
 
 def is_grande_suite(dice_roll):
     sorted_roll = sorted(dice_roll)
-    return sorted_roll == [1, 2, 3, 4, 5] or sorted_roll == [2, 3, 4, 5, 6]
+    if sorted_roll == [1, 2, 3, 4, 5] or sorted_roll == [2, 3, 4, 5, 6]:
+        return 40
+    return 0
 
 def is_chance(dice_roll):
     return sum(dice_roll)
--- yams_test.py
+++ yams_test.py
@@ -5,21 +5,23 @@ from yams import roll_dice, is_yams, is_brelan, is_carre, is_full, is_grande_sui
 
 def test_is_yams():
     assert is_yams(roll_dice(0)) == 50
+    assert not is_yams(roll_dice(9)) == 0
 
 def test_is_brelan():
-    assert is_brelan(roll_dice(7)) == 1
+    assert is_brelan(roll_dice(7)) == 28
+    assert not is_brelan()
 
 def test_is_carre():
-    assert is_carre(roll_dice(1)) == 1
+    assert is_carre(roll_dice(1)) == 36
 
 
 def test_is_full():
-    assert is_full(roll_dice(2))
+    assert is_full(roll_dice(2)) == 30
 
 
 def test_is_grande_suite():
-    assert is_grande_suite(roll_dice(5))
-    assert is_grande_suite(roll_dice(6))
+    assert is_grande_suite(roll_dice(5)) == 40
+    assert is_grande_suite(roll_dice(6)) == 40
 
 def test_score_chance():
     assert is_chance([1, 2, 3, 4, 5]) == 15
